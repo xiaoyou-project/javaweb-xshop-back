3f783d2ca482aa0828e0ccec41c0402bfc83f549
葛昊 <494542684@qq.com>
2020-06-03 14:39:40 +0800

    idea配置git

--- src/main/java/com/javaweb/ks/controller/ShopController.java
+++ src/main/java/com/javaweb/ks/controller/ShopController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 商品controller
+ */
 @Controller
 @Slf4j
 @RequestMapping("api/v1/shop")
